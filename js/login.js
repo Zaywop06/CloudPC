ef04ab5d6ab22783d46520bdabd6154cfef8df85
Alan Franco Pulido <alanfrancopulido@MacBook-Pro-de-Alan.local>
2024-02-27 19:18:06 -0600

    feat(cloudpc): Se crea login

---
+++ js/login.js
@@ -0,0 +1,28 @@
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+// validacion de la contraseña 
+function matchPassword() {  
+  var pw1 = document.getElementById("pswd1");  
+  var pw2 = document.getElementById("pswd2");  
+  if(pw1 != pw2)  
+  {   
+    alert("Las contraeñas no coinciden");  
+  } else  {  
+    if(pw1 = pw2){
+      alert("Cuenta creada con exito");  
+
+    }  
+  }
+  
+}  
